f9ed011a4b3cac0e834aa19069914c2c69ecbc8d
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-08-04 13:34:17 +0200

    Lots of improvements on the groups list

--- app/screens/Groups/index.js
+++ app/screens/Groups/index.js
@@ -1,9 +1,12 @@
 import React             from 'react'
 import { FireComponent } from 'fireflux'
+import t                 from 'tcomb-form'
 import Header            from 'shared/components/Header'
-import Group             from './components/Group'
+import nav               from 'shared/utils/nav'
 
-@FireComponent({ groups : '/groups' })
+let Form = t.form.Form
+
+@FireComponent({ groups : '/groups', events : '/events' })
 export default class Groups extends React.Component {
     constructor(props) {
         super(props)
@@ -12,20 +15,68 @@ export default class Groups extends React.Component {
         }
     }
     render() {
-        console.log(this.state)
         let groups = [];
+
+        // If no groups
+
         if (!this.state.adding && !this.state.groups) 
             groups.push(
                 <div key="nada">No groups found...</div>
             )
-        if (this.state.adding) groups.unshift(
-           <Group key="adding"
-                  onAddCancel={this.onAddCancel.bind(this)}/>
-        )
+
+        // Taken groups
+
+        let takenGroups = Object.keys(this.state.groups || {}).reduce((groups, id) => {
+            let g = this.state.groups[id]
+            if (groups.indexOf(g.id) >= 0) return groups
+            groups.push(g.id)
+            return groups 
+        }, [])
+ 
+        // Available groups
+
+        let allGroups = Object.keys(this.state.events || {}).reduce((groups, id) => {
+            let e = this.state.events[id]
+            if (groups.indexOf(e.id) >= 0) return groups
+            groups.push(e.id)
+            return groups
+        }, [])
+
+        // Available groups
+
+        let availableGroups = allGroups.filter((g) => {
+            return takenGroups.indexOf(g) < 0
+        })
+
+        console.log(takenGroups, allGroups, availableGroups)
+
+        // <Form> Available groups
+
+        let availableGroupsEnum = availableGroups.reduce((ag, g) => {
+            ag[g] = g
+            return ag
+        }, {})
+
+        let GroupForm = t.struct({
+            group : t.enums(availableGroupsEnum)
+        })
+
+        // <Group> Components
+
+        takenGroups.forEach((g) => {
+            groups.push(
+                <div className="Group">
+                    <h1>{g}</h1>
+                    <button onClick={this.viewGroup.bind(this,g)}>View</button>
+                </div>
+            )
+        })
+
         return (
             <div className="GroupsScreen">
                 <Header />
                 <div className="addButton">
+                    <Form ref="form" type={GroupForm} />
                     <button onClick={this.onAddClick.bind(this)}>+</button>
                 </div>
                 <div className="groups">
@@ -35,14 +86,15 @@ export default class Groups extends React.Component {
         )
     }
     onAddClick() {
-        if (this.state.adding) return
-        this.setState({adding:true})
+        let value = this.refs.form.getValue()
+        if (!value) return
+        this.ref.child('/groups').push({
+            id   : value.group,
+            diff : 5,
+            unit : 'minute'
+        })
     }
-    onAddSave(values) {
-        console.log('saving',values)
-    }
-    onAddCancel() {
-        if (!this.state.adding) return
-        this.setState({adding:false})
+    viewGroup(g) {
+        nav.navigate('/groups/'+g)
     }
 }
