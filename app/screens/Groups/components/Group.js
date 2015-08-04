ff255b46b8e33c160f90f6911d64f8a90a2cf2fc
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-08-04 10:50:23 +0200

    Some work

--- app/screens/Groups/components/Group.js
+++ app/screens/Groups/components/Group.js
@@ -2,6 +2,7 @@ import React         from 'react'
 import t             from 'tcomb-form'
 import FluxComponent from 'flummox/component'
 import Header        from 'shared/components/Header'
+import nav           from 'shared/utils/nav'
 
 let Form = t.form.Form
 let GridItemForm = t.struct({
@@ -12,12 +13,12 @@ let GridItemForm = t.struct({
 
 export default class GridItem extends React.Component {
     render() {
-        let value = this.props.grid ? this.props.grid.get('meta').toJS() : {}
-        let middleButton = this.props.grid ?
+        let value = this.props.group || {}
+        let middleButton = this.props.group ?
             <button onClick={this.onEdit.bind(this)}>Edit Items</button> :
             <button onClick={this.onCancel.bind(this)}>Cancel</button>
         return (
-            <div className="GridItem">
+            <div className="Group">
                 <Form ref="form" type={GridItemForm} value={value} />
                 <div className="buttons">
                     <button onClick={this.onSave.bind(this)}>Save</button>
@@ -31,17 +32,17 @@ export default class GridItem extends React.Component {
         let values = this.refs.form.getValue()
         if (!values) return
         this.props.onAddCancel()
-        if (!this.props.grid) return this.props.flux.getActions('grid').add(values)
-        this.props.flux.getActions('grid').update(this.props.grid.get('meta').merge(values))
+        if (!this.props.grid) return this.props.onAddSave(values)
+//        this.props.flux.getActions('grid').update(this.props.grid.get('meta').merge(values))
     }
     onCancel() {
         if (!this.props.grid) return this.props.onAddCancel()
     }
     onRemove() {
         if (!this.props.grid) return this.props.onAddCancel()
-        this.props.flux.getActions('grid').remove(this.props.grid)
+//        this.props.flux.getActions('grid').remove(this.props.grid)
     }
     onEdit() {
-        this.props.flux.getUtils('nav').navigate('/grid/'+this.props.grid.get('meta').get('id'))
+        nav.navigate('/grid/'+this.props.group.id)
     }
 }
--- app/screens/Groups/index.js
+++ app/screens/Groups/index.js
@@ -1,7 +1,7 @@
 import React             from 'react'
 import { FireComponent } from 'fireflux'
 import Header            from 'shared/components/Header'
-import GridsList         from './components/GridsList'
+import Group             from './components/Group'
 
 @FireComponent({ groups : '/groups' })
 export default class Groups extends React.Component {
@@ -13,11 +13,23 @@ export default class Groups extends React.Component {
     }
     render() {
         console.log(this.state)
+        let groups = [];
+        if (!this.state.adding && !this.state.groups) 
+            groups.push(
+                <div>No groups found...</div>
+            )
+        if (this.state.adding) groups.unshift(
+           <Group key="adding"
+                  onAddCancel={this.onAddCancel.bind(this)}/>
+        )
         return (
             <div className="GroupsScreen">
                 <Header />
                 <div className="addButton">
                     <button onClick={this.onAddClick.bind(this)}>+</button>
+                </div>
+                <div className="groups">
+                    {groups}
                 </div>
             </div>
         )
@@ -25,6 +37,9 @@ export default class Groups extends React.Component {
     onAddClick() {
         if (this.state.adding) return
         this.setState({adding:true})
+    }
+    onAddSave(values) {
+        console.log('saving',values)
     }
     onAddCancel() {
         if (!this.state.adding) return
